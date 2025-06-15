a4af0895dcba873f98f66e29f5499e0a0378420c
Nadja Galih <nadjagalih004@student.ub.ac.id>
2025-06-15 12:14:12 +0700

    Update and rename 2025_06_14_042021_create_drivers_table.php to 2025_06_14_042021_create_orderers_table.php

--- database/migrations/2025_06_14_042021_create_orderers_table.php
+++ database/migrations/2025_06_14_042021_create_orderers_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('drivers', function (Blueprint $table) {
+        Schema::create('orderers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('phone_number')->nullable();
